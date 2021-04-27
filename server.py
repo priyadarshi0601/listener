23676bfdbf2d7369ba84a277b93c07cdb68ef6c0
priyadarshi0601 <49425913+priyadarshi0601@users.noreply.github.com>
2021-04-28 00:42:29 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from os import environ
 from flask import Flask
 from flask import request
 app = Flask(__name__)
-@app.route('/listen')
+@app.route('/listen',methods=['GET','POST'])
 def home():
     print(request.get_json)
     return('Hello')
