90028785c651595aa4024aab2e25f044bc245012
priyadarshi0601 <49425913+priyadarshi0601@users.noreply.github.com>
2021-04-28 00:51:13 +0530

    Update server.py

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route('/listen',methods=['GET','POST'])
 def home():
     print(request.get_json)
+    print('FORM DATA',request.form)
     return('Hello')
 if __name__=="__main__":
     app.run(host='0.0.0.0',debug=True)
